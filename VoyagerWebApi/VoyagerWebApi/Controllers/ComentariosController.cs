edd179ca6617b58cce493ffdb3616d46de48907f
Guilherme Henrique Garbelini Alberto <46561309820@info.local>
2024-06-06 15:51:26 -0300

    comentarios fazendo

---
+++ VoyagerWebApi/VoyagerWebApi/Controllers/ComentariosController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VoyagerWebApi.Domains;
+using VoyagerWebApi.Interfaces;
+using VoyagerWebApi.Repositories;
+using VoyagerWebApi.ViewModels;
+
+namespace VoyagerWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ComentariosController : ControllerBase
+    {
+        private readonly IComentariosRepository _comentariosRepository;
+        
+        public ComentariosController()
+        {
+            _comentariosRepository = new ComentariosRepository();
+        }
+
+        [HttpGet("{idPostagem}")]
+        public IActionResult BuscarPorPostagem(Guid idPostagem)
+        {
+            try
+            {
+                List<Comentarios> listaDeComentarios = _comentariosRepository.ListarPorPostagem(idPostagem);
+
+                return Ok(listaDeComentarios);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.Message);
+            }
+        }
+
+        [HttpPost("{idPostagem}")]
+        public IActionResult Cadastrar(Guid idPostagem)
+        {
+            try
+            {
+                
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.Message);
+            }
+        }
+
+        [HttpDelete("{idComentario}")]
+        public IActionResult Delete(Guid idComentario)
+        {
+            try
+            {
+                Comentarios comentarioBuscado = _comentariosRepository.BuscarPorId(idComentario);
+
+                if (comentarioBuscado == null)
+                {
+                    return NotFound("Atividade não encontrada");
+                }
+
+                return Ok(comentarioBuscado);
+            }
+            catch (Exception erro)
+            {
+                return BadRequest(erro.Message);
+            }
+        }
+    }
+}
---
+++ VoyagerWebApi/VoyagerWebApi/Interfaces/IComentariosRepository.cs
@@ -0,0 +1,12 @@
+﻿using VoyagerWebApi.Domains;
+
+namespace VoyagerWebApi.Interfaces
+{
+    public interface IComentariosRepository
+    {
+        void Cadastrar(Comentarios novoComentario);
+        void Deletar (Guid idComentario);
+        Comentarios BuscarPorId(Guid idComentario);
+        List<Comentarios> ListarPorPostagem(Guid idPostagem);
+    }
+}
